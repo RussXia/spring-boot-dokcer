0b0469b90b1312e3a6a949d64e3ba6c743b523c1
RuzzZZ <xzy199400@gmail.com>
2020-01-07 17:53:54 +0800

    add router

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiazhengyue
+ * @since 2020-01-07
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping(value = "/sayHi", method = RequestMethod.GET)
+    public String sayHi(String name) {
+        return "Hello," + name;
+    }
+}
